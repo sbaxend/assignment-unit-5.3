b634fc29a98e7001434b77c38d0640261e296bb2
Seth Baxendell <Sethbaxendell2013@hotmail.com>
2023-02-05 13:59:33 -0500

    got the show collection function working

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -39,3 +39,17 @@ console.log(addToCollection('DAMN', 'Kendrick Lamar', 2017));
 console.log(addToCollection('good kid, m.A.A.d city', 'Kendrick Lamar', 2012));
 console.table(collection);
 
+//Add a function named `showCollection`. This function should:
+//Take in an array parameter. (This allows it to be reused to show any collection, 
+//like the results from the find or search.)
+//Console.log the number of items in the array.
+//Loop over the array and console.log each album's information formatted like:
+// `TITLE by ARTIST, published in YEAR`.
+
+function showCollection(array) {
+    console.log(`Number of albums in collection: ${array.length}`);
+    for (let album of array) {
+      console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+    }
+  };
+console.log(showCollection(collection));

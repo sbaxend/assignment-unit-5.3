a1f593319366643ea08d792bf622b28ee0157238
Seth Baxendell <Sethbaxendell2013@hotmail.com>
2023-02-05 12:09:14 -0500

    declared collection as a variable and initial start of a funciton addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,13 @@
 console.log('***** Music Collection *****')
+
+//- Add a function named `addToCollection`. This function should:
+//- Take in the album's `title`, `artist`, `yearPublished` as input parameters
+//- Create a new object having the above properties
+//- Add the new object to the end of the `collection` array
+//- Return the newly created object
+
+let collection = [];
+
+function addToCollection() {
+
+}
